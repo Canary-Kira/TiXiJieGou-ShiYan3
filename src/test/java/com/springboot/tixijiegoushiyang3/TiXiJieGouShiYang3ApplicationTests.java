1f55cca55a1d9a4b1563ecf20406ccb3b1e01c06
Canary-Kira <157281469+Canary-Kira@users.noreply.github.com>
2024-10-24 09:57:54 +0800

    初始化项目

---
+++ src/test/java/com/springboot/tixijiegoushiyang3/TiXiJieGouShiYang3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tixijiegoushiyang3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiXiJieGouShiYang3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
